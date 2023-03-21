1588b27a2207fd84c5a2164f8029fe625eb9521f
Salma Alami <alamisalma949@gmail.com>
2023-03-21 13:06:35 +0100

    vSalma

--- resources/views/admin/paginaPrincipal.blade.php
+++ resources/views/admin/paginaPrincipal.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.layout')
 @section('content')
     <main>
-        <div class="ff" style="border:1px solid;width:20%;margin:auto;">
+        <h1>BIKE ROLL</h1>
+        {{-- <div class="ff" style="border:1px solid;width:20%;margin:auto;">
             <div id="div1" style="border:1px solid;width:100%;text-align:center">
                 <h3>Gestionar carreras</a></h3>
                 <p><a href="{{url('anyadirCarrera')}}">Añadir carrera</a></p>
@@ -23,9 +24,56 @@
             <div style="border:1px solid;width:100%;text-align:center">
                 <h3>Corredores apuntados</h3>
                 <p><a href="{{url('verCorredores')}}">Ver corredores apuntados</a></p>
-            </div>
+            </div> --}}
     
         </div>
         <h3><a href="{{url('logout')}}">Cerrar sesión</a></h3>
     </main>
+    <div class="container">
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Gestionar carreras</text></svg>
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver todas</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Añadir</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Gestionar aseguradoras</text></svg>
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver todas</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Añadir</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Gestionar sponsors</text></svg>
+              <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver todas</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Añadir</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Editar</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
